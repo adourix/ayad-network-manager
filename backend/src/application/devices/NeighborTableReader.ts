6c6ebcd5551049194b168882d14b939ca633fb46
adourix <137846847+adourix@users.noreply.github.com>
2026-09-08 06:33:56 +0300

    refactor: remove duplicate neighbor port

--- backend/src/application/devices/NeighborTableReader.ts
+++
@@ -1,9 +0,0 @@
-export interface NeighborEntry {
-  ip: string;
-  mac: string;
-  state: string;
-}
-
-export interface NeighborTableReader {
-  read(interfaceName: string): Promise<NeighborEntry[]>;
-}
